1ab9a4412a6efb459462de924bfffe533202da69
Jessica Karpovich <karpovichjm@gmail.com>
2019-03-08 19:50:10 -0600

    Initial Commit

---
+++ swift-serial/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-serial
+//
+//  Created by Jessica Karpovich on 3/8/19.
+//  Copyright © 2019 Jessica Karpovich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
